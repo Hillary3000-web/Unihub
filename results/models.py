fe38a2ec94208cca4e4d1348029c6b12dab74c4b
Hillary Chukwuma Prince <princehillary813@gmail.com>
2026-02-15 20:39:48 +0100

    feat: initial UniHub setup with Django backend and project structure

--- results/models.py
+++ results/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+# For my Resource Hub
+class StudyMaterial(models.Model):
+    title = models.CharField(max_length=255)
+    file = models.FileField(upload_to='materials/')
+    uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+# For my Announcements System
+class Announcement(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    is_urgent = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
--- results/views.py
+++ results/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from .models import Announcement
+from .serializers import AnnouncementSerializer
+
+class AnnouncementList(generics.ListCreateAPIView):
+    queryset = Announcement.objects.all()
+    serializer_class = AnnouncementSerializer
